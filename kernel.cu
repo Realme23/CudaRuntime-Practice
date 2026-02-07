5df8ab063a3c3091b84dfb08af1868b273fcd2d6
CatsFan <CatsFan@DESKTOP-H4K0GGF>
2026-02-07 21:52:41 +0530

    Exercise 3.1.b -- 1 thread / element

--- kernel.cu
+++ kernel.cu
@@ -6,6 +6,8 @@
 #include <vector>
 #include <chrono>
 
+__host__ cudaError_t MatrixAdd(float* A, const float* B, const float* C, int size);
+
 float nextRand() {
     static unsigned int seed = std::chrono::steady_clock::now().time_since_epoch().count();
     seed = (seed * 1664525 + 1013904223);
@@ -14,12 +16,50 @@ float nextRand() {
 
 int main()
 {
+    constexpr int size = 16;
+    //Allocate vectors
+    float* A = new float[size * size];
+    float* B = new float[size * size];
+    float* C = new float[size * size];
 
+    //Fill up vectors
+    for (int i = 0; i < size * size; i++) {
+        A[i] = 0;
+        B[i] = 2 * i % 3;
+        C[i] = i % 7;
+    }
 
+    for (int i = 0; i < size * size; i++) {
+        std::cout << C[i] << ' ';
+        if (i % size == (size - 1))
+            std::cout << '\n';
+    }
+    std::cout << '\n';
+
+    for (int i = 0; i < size * size; i++) {
+        std::cout << B[i] << ' ';
+        if (i % size == (size - 1))
+            std::cout << '\n';
+    }
+    std::cout << '\n';
+
+    //Call the function
+    cudaError_t cudaStatus = MatrixAdd(A, B, C, size);
+    if (cudaStatus != cudaSuccess) {
+        std::clog << "Failed to run!\n";
+        return -1;
+    }
+
+    //Display results
+    for (int i = 0; i < size * size; i++) {
+        std::cout << A[i] << ' ';
+        if (i % size == (size - 1))
+            std::cout << '\n';
+    }
 
     // cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaError_t cudaStatus = cudaDeviceReset();
+    cudaStatus = cudaDeviceReset();
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaDeviceReset failed!");
         return 1;
@@ -28,9 +68,26 @@ int main()
     return 0;
 }
 
+//Device Kernel
+__global__ void addMatrix(float* A, const float* B, const float* C, int size) {
+    //Which thread is this?
+    int threadC = threadIdx.x + (blockIdx.x * blockDim.x);
+    //If the thread is > than the matrix, skip
+    if (threadC > size * size) {
+        return;
+    }
+    else {
+        //Get the i and j for the thread
+        int i = threadC % size;
+        int j = threadC / size;
+        //Add the element for that thread
+        A[i + j * size] = B[i + j * size] + C[i + j * size];
+    }
+}
+
 //Add two 2D matrices
 //Parameters: A = Result Matrix, B = Operand 1, C = Operand 2, size = number of elements _per dimension_
-__host__ cudaError_t MatrixMultiply(float* A, const float* B, const float* C, int size) {
+__host__ cudaError_t MatrixAdd(float* A, const float* B, const float* C, int size) {
 
     size_t bytesSize = size * size * sizeof(float);
 
@@ -48,7 +105,8 @@ __host__ cudaError_t MatrixMultiply(float* A, const float* B, const float* C, in
     result = cudaMemcpy(deviceC, C, bytesSize, cudaMemcpyHostToDevice);
     if (result != cudaSuccess) goto Error;
 
-    addMatrix << < >> > (deviceA, deviceB, deviceC, size);
+    //Add with 256 threads per block and (size * size)/256 blocks for a total of (size * size) threads
+    addMatrix<<<ceil(size * size / 256.0),256>>>(deviceA, deviceB, deviceC, size);
 
     result = cudaMemcpy(A, deviceA, bytesSize, cudaMemcpyDeviceToHost);
 
