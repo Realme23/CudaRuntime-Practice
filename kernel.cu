fcdcffc304d1b0cc6f91c1962b8f58c3c3c50222
Realme23 <67191936+Realme23@users.noreply.github.com>
2026-02-18 22:28:33 +0530

    Increased kernel size

--- kernel.cu
+++ kernel.cu
@@ -14,21 +14,21 @@
 #include <algorithm>
 #include <ranges>
 
-void addWithGPU(float* c, const float* a, const float* b, unsigned int size);
-void addWithGPUSetup(float* c, const float* a, const float* b, unsigned int size, float*&, float*&, float*&);
-void addWithGPUFinish(float* c, const float* a, const float* b, unsigned int size, float*&, float*&, float*&);
-void addWithCPUScalar(float c[], const float a[], const float b[], unsigned int size);
-void addWithCPU4Vector(float* c, const float* a, const float* b, unsigned int size);
-void addWithCPU8Vector(float* c, const float* a, const float* b, unsigned int size);
-void addWithCPUMulti8Vector(float* c, const float* a, const float* b, unsigned int size);
-
 __global__ void addKernel(float* c, const float* a, const float* b, unsigned int size)
 {
     int i = (blockIdx.x * blockDim.x) + threadIdx.x;
-    if (i < size)
+    if (i < size) {
         c[i] = a[i] + b[i];
-    else
-        c[i] = 1;
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+    }
 }
 
 float nextRand() {
@@ -39,148 +39,20 @@ float nextRand() {
     return ((seed) / 1.0f);
 }
 
-int main()
-{
-    const int arraySizeRaw = (1 << 28);
-    const int arraySize = arraySizeRaw - (arraySizeRaw % (8 * 64));
-    float* a = new alignas(32) float[arraySize];
-    float* b = new alignas(32) float[arraySize];
-    float* c = new alignas(32) float[arraySize];
-    float* check = new alignas(32) float[arraySize];
-
-#pragma loop(hint_parallel(0))
-#pragma loop(ivdep)
-    for (int i = 0; i < arraySize; i++) {
-        a[i] = nextRand();
-        b[i] = nextRand();
-        check[i] = a[i] + b[i];
-    }
-
-    std::chrono::steady_clock::time_point now;
-    std::chrono::nanoseconds duration;
-
-    std::cout << "Begin bench" << '\n';
-    std::cout << "Scalars:\n";
-
-
-    now = std::chrono::steady_clock().now();
-    for (int i = 0; i < 10; i++) {
-        addWithCPUScalar(c, a, b, arraySize);
-    }
-    duration = std::chrono::steady_clock().now() - now;
-
-    for (int i = 0; i < arraySize; i++) {
-        if (c[i] != check[i])
-            std::clog << "Returned values do not match! " << i << " " << c[i] << ", " << check[i];
-        c[i] = 0;
-    }
-    std::cout << "Duration was: " << duration.count() / 10 << '\n';
-
-
-
-    std::cout << "Begin bench" << '\n';
-    std::cout << "4Vectors:\n";
-    now = std::chrono::steady_clock().now();
-
-    for (int i = 0; i < 10; i++) {
-        addWithCPU4Vector(c, a, b, arraySize);
-    }
-    duration = std::chrono::steady_clock().now() - now;
-    for (int i = 0; i < arraySize; i++) {
-        if (c[i] != check[i])
-            std::clog << "Returned values do not match! " << i << " " << c[i] << ", " << check[i];
-        c[i] = 0;
-    }
-
-    std::cout << "Duration was: " << duration.count() / 10 << '\n';
-
-    std::cout << "Begin bench" << '\n';
-    std::cout << "8Vectors:\n";
-    now = std::chrono::steady_clock().now();
-
-    for (int i = 0; i < 10; i++) {
-        addWithCPU8Vector(c, a, b, arraySize);
-    }
-    duration = std::chrono::steady_clock().now() - now;
-    for (int i = 0; i < arraySize; i++) {
-        if (c[i] != check[i])
-            std::clog << "Returned values do not match! " << i << " " << c[i] << ", " << check[i];
-        c[i] = 0;
-    }
-    std::cout << "Duration was: " << duration.count() / 10 << '\n';
-
-    std::cout << "Begin bench" << '\n';
-    std::cout << "8Vectors Multithreaded Setup:" << '\n';
-    now = std::chrono::steady_clock().now();
-
-    duration = std::chrono::steady_clock().now() - now;
-    std::cout << "Duration was: " << duration.count() / 10 << '\n';
-
-    std::cout << "Begin bench" << '\n';
-    std::cout << "8Vectors Multithreaded:" << '\n';
-
-    now = std::chrono::steady_clock().now();
-    for (unsigned int i = 0; i < 10; i++) {
-        addWithCPUMulti8Vector(c, a, b, arraySize);
-    }
-    duration = std::chrono::steady_clock().now() - now;
-    for (int i = 0; i < arraySize; i++) {
-        if (c[i] != check[i])
-            std::clog << "Returned values do not match! " << i << " " << a[i] << " " << b[i] << " " << c[i] << ", " << check[i] << '\n';
-        c[i] = 2;
-    }
-    std::cout << "Duration was: " << duration.count() / 10 << '\n';
-
-
-    std::cout << "Begin bench" << '\n';
-    std::cout << "CUDA Setup and return:\n";
-    now = std::chrono::steady_clock().now();
-
-    float* deviceA, * deviceB, * deviceC;
-    addWithGPUSetup(c, a, b, arraySize, deviceA, deviceB, deviceC);
-    duration = std::chrono::steady_clock().now() - now;
-
-    std::cout << "Duration was: " << duration.count() << '\n';
-    auto cd_setup = duration;
-
-    std::cout << "Begin bench" << '\n';
-    std::cout << "CUDA addition:\n";
-    now = std::chrono::steady_clock().now();
-    for (int i = 0; i < 10; i++) {
-        addWithGPU(deviceC, deviceA, deviceB, arraySize);
-    }
-    duration = std::chrono::steady_clock().now() - now;
-    addWithGPUFinish(c, a, b, arraySize, deviceA, deviceB, deviceC);
-
-    for (int i = 0; i < arraySize; i++) {
-        if (c[i] != check[i])
-            std::clog << "Returned values do not match! " << i << " " << std::bit_cast<unsigned int>(c[i]) << ", " << check[i];
-        c[i] = 0;
-    }
-
-    std::cout << "Duration was: " << duration.count() / 10 << '\n';
-
-    cudaFree(deviceA);
-    cudaFree(deviceB);
-    cudaFree(deviceC);
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaError_t cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
-
-    delete[] a, b, c;
-
-    return 0;
-}
-
-void addWithCPUScalar(float c[], const float a[], const float b[], unsigned int size) {
+void addWithCPUScalar(volatile float c[], volatile const float a[], volatile const float b[], volatile unsigned int size) {
 #pragma loop(no_parallel)
 #pragma loop(no_vector)
     for (int i = 0; i < (int)size; i++) {
         c[i] = a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
+        c[i] += a[i] + b[i];
     }
     return;
 }
@@ -192,7 +64,19 @@ void addWithCPU4Vector(float* c, const float* a, const float* b, unsigned const 
         a_w = _mm_load_ps(a + i * 4);
         b_w = _mm_load_ps(b + i * 4);
         c_w = _mm_add_ps(a_w, b_w);
-        _mm_stream_ps(c + i * 4, c_w);
+
+        __m128 s_w = _mm_setzero_ps();
+        s_w = _mm_add_ps(s_w, c_w);
+        s_w = _mm_add_ps(s_w, c_w);
+        s_w = _mm_add_ps(s_w, c_w);
+        s_w = _mm_add_ps(s_w, c_w);
+        s_w = _mm_add_ps(s_w, c_w);
+        s_w = _mm_add_ps(s_w, c_w);
+        s_w = _mm_add_ps(s_w, c_w);
+        s_w = _mm_add_ps(s_w, c_w);
+        s_w = _mm_add_ps(s_w, c_w);
+        s_w = _mm_add_ps(s_w, c_w);
+        _mm_stream_ps(c + i*4, s_w);
     }
     return;
 }
@@ -204,7 +88,18 @@ void addWithCPU8Vector(float* c, const float* a, const float* b, unsigned const 
         a_w = _mm256_load_ps(a + i * 8);
         b_w = _mm256_load_ps(b + i * 8);
         c_w = _mm256_add_ps(a_w, b_w);
-        _mm256_stream_ps(c + i * 8, c_w);
+        __m256 s_w = _mm256_setzero_ps();
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        _mm256_stream_ps(c + i * 8, s_w);
     }
     return;
 }
@@ -217,7 +112,18 @@ void addWithCPUMulti8Vector(float* c, const float* a, const float* b, unsigned i
         a_w = _mm256_load_ps(a + i * 8);
         b_w = _mm256_load_ps(b + i * 8);
         c_w = _mm256_add_ps(a_w, b_w);
-        _mm256_stream_ps(c + i * 8, c_w);
+        __m256 s_w = _mm256_setzero_ps();
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        s_w = _mm256_add_ps(s_w, c_w);
+        _mm256_stream_ps(c + i * 8, s_w);
     }
     return;
 }
@@ -258,6 +164,7 @@ Error:
 
 void addWithGPU(float* deviceC, float const* deviceA, float const* deviceB, unsigned int size) {
     addKernel << <ceil(size / 1024), 1024 >> > (deviceC, deviceA, deviceB, size);
+    cudaDeviceSynchronize();
     cudaError_t result = cudaGetLastError();
     if (result != cudaSuccess) {
         std::clog << "Cuda Error! Switching off now." << '\n';
@@ -265,4 +172,144 @@ void addWithGPU(float* deviceC, float const* deviceA, float const* deviceB, unsi
     }
     cudaDeviceSynchronize();
     return;
+}
+
+
+int main()
+{
+    const int arraySizeRaw = (1 << 28);
+    const int arraySize = arraySizeRaw - (arraySizeRaw % (8 * 64));
+    float* a = new alignas(32) float[arraySize];
+    float* b = new alignas(32) float[arraySize];
+    float* c = new alignas(32) float[arraySize];
+    float* check = new alignas(32) float[arraySize];
+
+#pragma loop(hint_parallel(8))
+#pragma loop(ivdep)
+    for (int i = 0; i < arraySize; i++) {
+        a[i] = nextRand();
+        b[i] = nextRand();
+        check[i] = a[i] + b[i];
+        check[i] += a[i] + b[i];
+        check[i] += a[i] + b[i];
+        check[i] += a[i] + b[i];
+        check[i] += a[i] + b[i];
+        check[i] += a[i] + b[i];
+        check[i] += a[i] + b[i];
+        check[i] += a[i] + b[i];
+        check[i] += a[i] + b[i];
+        check[i] += a[i] + b[i];
+    }
+
+    std::chrono::steady_clock::time_point now;
+    std::chrono::nanoseconds duration;
+    int loop_count = 1;
+
+    std::cout << "Begin bench" << '\n';
+    std::cout << "Scalars:\n";
+
+    loop_count = 5;
+    now = std::chrono::steady_clock().now();
+    for (int i = 0; i < loop_count; i++) {
+        addWithCPUScalar(c, a, b, arraySize);
+    }
+    duration = std::chrono::steady_clock().now() - now;
+
+    for (int i = 0; i < arraySize; i++) {
+        if (c[i] != check[i])
+            std::clog << "Returned values do not match! " << i << " " << c[i] << ", " << check[i];
+        c[i] = 0;
+    }
+    std::cout << "Duration was: " << duration.count() / loop_count << '\n';
+
+    loop_count = 100;
+    std::cout << "Begin bench" << '\n';
+    std::cout << "4Vectors:\n";
+    now = std::chrono::steady_clock().now();
+    for (int i = 0; i < loop_count; i++) {
+        addWithCPU4Vector(c, a, b, arraySize);
+    }
+    duration = std::chrono::steady_clock().now() - now;
+    for (int i = 0; i < arraySize; i++) {
+        if (c[i] != check[i])
+            std::clog << "Returned values do not match! " << i << " " << c[i] << ", " << check[i];
+        c[i] = 0;
+    }
+
+    std::cout << "Duration was: " << duration.count() / loop_count << '\n';
+
+    std::cout << "Begin bench" << '\n';
+    std::cout << "8Vectors:\n";
+    now = std::chrono::steady_clock().now();
+
+    for (int i = 0; i < loop_count; i++) {
+        addWithCPU8Vector(c, a, b, arraySize);
+    }
+    duration = std::chrono::steady_clock().now() - now;
+    for (int i = 0; i < arraySize; i++) {
+        if (c[i] != check[i])
+            std::clog << "Returned values do not match! " << i << " " << c[i] << ", " << check[i];
+        c[i] = 0;
+    }
+    std::cout << "Duration was: " << duration.count() / loop_count << '\n';
+
+
+    std::cout << "Begin bench" << '\n';
+    std::cout << "8Vectors Multithreaded:" << '\n';
+
+    now = std::chrono::steady_clock().now();
+    for (unsigned int i = 0; i < loop_count; i++) {
+        addWithCPUMulti8Vector(c, a, b, arraySize);
+    }
+    duration = std::chrono::steady_clock().now() - now;
+    for (int i = 0; i < arraySize; i++) {
+        if (c[i] != check[i])
+            std::clog << "Returned values do not match! " << i << " " << a[i] << " " << b[i] << " " << c[i] << ", " << check[i] << '\n';
+        c[i] = 2;
+    }
+    std::cout << "Duration was: " << duration.count() / loop_count << '\n';
+
+
+    std::cout << "Begin bench" << '\n';
+    std::cout << "CUDA Setup and return:\n";
+    now = std::chrono::steady_clock().now();
+
+    float* deviceA, * deviceB, * deviceC;
+    addWithGPUSetup(c, a, b, arraySize, deviceA, deviceB, deviceC);
+    duration = std::chrono::steady_clock().now() - now;
+
+    std::cout << "Duration was: " << duration.count() << '\n';
+    auto cd_setup = duration;
+
+    std::cout << "Begin bench" << '\n';
+    std::cout << "CUDA addition:\n";
+    now = std::chrono::steady_clock().now();
+    for (int i = 0; i < loop_count; i++) {
+        addWithGPU(deviceC, deviceA, deviceB, arraySize);
+    }
+    duration = std::chrono::steady_clock().now() - now;
+    addWithGPUFinish(c, a, b, arraySize, deviceA, deviceB, deviceC);
+
+    for (int i = 0; i < arraySize; i++) {
+        if (c[i] != check[i])
+            std::clog << "Returned values do not match! " << i << " " << c[i] << ", " << check[i];
+        c[i] = 0;
+    }
+
+    std::cout << "Duration was: " << duration.count() / loop_count << '\n';
+
+    cudaFree(deviceA);
+    cudaFree(deviceB);
+    cudaFree(deviceC);
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    cudaError_t cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    delete[] a, b, c;
+
+    return 0;
 }
